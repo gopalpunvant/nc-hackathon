0931965843c0be542028d661f26c1c4dd48b629a
Gopal Puvantwar <gopu0721@ws-6786.netcracker.com>
2023-08-27 14:09:35 +0530

    Input JSON is changed

--- hypertext-coder-FE/src/mermaid/designInput.js
+++ hypertext-coder-FE/src/mermaid/designInput.js
@@ -1,47 +1,75 @@
-export const design = [
-    { 
-        name: 'view_configuration', 
-        properties: ['+object CustomerInfo', '+object AddressInfo', '+object OrderInfo', '+object Subscriptions'], 
-        methods: ['+onSubmit()'] 
-    },
-    { 
-        name: 'CustomerDetails', 
-        properties: ['+object CustomerInfo', '+boolean showCustomerInfo'], 
-        methods: ['+onBlur()', '+onClick()'], 
-        parent: "view_configuration", 
-        props: "CustomerInfo" 
-    },
-    { 
-        name: 'CustomerPersonalDetails', 
-        properties: ['+string CustomerPhoneNumber', '+string CustomerEmailId'], 
-        methods: ['-onBlur()', '-onClick()'], 
-        parent: "CustomerDetails" 
-    },
-    { 
-        name: 'CustomerContactDetails', 
-        properties: ['+string Gender', '+string DOB', '+boolean showAddressDetails', '+number custContact'], 
-        methods: ['+onBlur()', '+onSubmit()'], 
-        parent: "CustomerDetails" 
-    },
-    { 
-        name: 'AddressDetails', 
-        properties: ['+object AddressInfo', '+boolean showAddressDetails'], 
-        methods: ['+onBlur()', '+onSubmit()'], 
-        parent: "view_configuration", 
-        props: "AddressInfo" 
-    },
-    { 
-        name: 'OrderDetails', 
-        properties: ['+object OrderInfo', '++boolean showOrderInfo'], 
-        methods: ['+onBlur()', '+onSubmit()'], 
-        parent: "view_configuration", 
-        props: "OrderInfo" 
-    },
-    { 
-        name: 'SubscriptionsDetails', 
-        properties: ['+object Subscriptions', '+boolean showSubscriptions'], 
-        methods: ['+onBlur()', '+onSubmit()'], 
-        parent: "view_configuration", 
-        props: "Subscriptions" 
-    },
-];
+export const input = {
+    design: [
+        {
+            name: 'view_configuration',
+            properties: ['+object CustomerInfo', '+object AddressInfo', '+object OrderInfo', '+object Subscriptions'],
+            methods: ['+onSubmit()']
+        },
+        {
+            name: 'CustomerDetails',
+            properties: ['+object CustomerInfo', '+boolean showCustomerInfo'],
+            methods: ['+onBlur()', '+onClick()'],
+            parent: "view_configuration",
+            props: "CustomerInfo"
+        },
+        {
+            name: 'CustomerPersonalDetails',
+            properties: ['+string CustomerPhoneNumber', '+string CustomerEmailId'],
+            methods: ['-onBlur()', '-onClick()'],
+            parent: "CustomerDetails"
+        },
+        {
+            name: 'CustomerContactDetails',
+            properties: ['+string Gender', '+string DOB', '+boolean showAddressDetails', '+number custContact'],
+            methods: ['+onBlur()', '+onSubmit()'],
+            parent: "CustomerDetails"
+        },
+        {
+            name: 'AddressDetails',
+            properties: ['+object AddressInfo', '+boolean showAddressDetails'],
+            methods: ['+onBlur()', '+onSubmit()'],
+            parent: "view_configuration",
+            props: "AddressInfo"
+        },
+        {
+            name: 'OrderDetails',
+            properties: ['+object OrderInfo', '++boolean showOrderInfo'],
+            methods: ['+onBlur()', '+onSubmit()'],
+            parent: "view_configuration",
+            props: "OrderInfo"
+        },
+        {
+            name: 'SubscriptionsDetails',
+            properties: ['+object Subscriptions', '+boolean showSubscriptions'],
+            methods: ['+onBlur()', '+onSubmit()'],
+            parent: "view_configuration",
+            props: "Subscriptions"
+        },
+    ],
+    code: {
+        "xtype": "h1",
+        "value": "Student Details",
+        "childrens": [
+            {
+                "xtype": "text",
+                "displayField": "Name",
+                "valueField": "name"
+            },
+            {
+                "xtype": "text",
+                "displayField": "Class",
+                "valueField": "class"
+            },
+            {
+                "xtype": "text",
+                "displayField": "School Name",
+                "valueField": "schoolName"
+            },
+            {
+                "xtype": "text",
+                "displayField": "Address",
+                "valueField": "address"
+            }
+        ]
+    }
+};
